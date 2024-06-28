90db800ea672ae4faed5c44d2c801dcf1c346f6c
Matthew Leibowitz <mattleibow@live.com>
2024-06-29 03:27:02 +0800

    mORE

--- Exciting.Mobile/ViewModels/TeamMemberViewModel.cs
+++ Exciting.Mobile/ViewModels/TeamMemberViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
 using Exciting.TeamModel;
+using SkiaSharp;
+using SkiaSharp.Views.Maui;
 
 namespace Exciting.Mobile;
 
@@ -26,6 +28,7 @@ public partial class TeamMemberViewModel(TeamMember member) : ObservableObject
 
     [ObservableProperty]
     [NotifyPropertyChangedFor(nameof(ProfilePicture))]
+    [NotifyPropertyChangedFor(nameof(ProfileColor))]
     byte[]? profilePictureBytes = member.ProfilePicture;
 
     public ObservableCollection<TaskItemViewModel> Tasks { get; private set; } = GetTasks(member.Tasks);
@@ -35,6 +38,8 @@ public partial class TeamMemberViewModel(TeamMember member) : ObservableObject
     public string Initial => FirstName.Length > 0 ? FirstName[..1] : "";
 
     public ImageSource? ProfilePicture => DecodeImage(ProfilePictureBytes, LastName);
+
+    public Color ProfileColor => DecodeColor(ProfilePictureBytes);
 
     public void RevertChanges()
     {
@@ -47,11 +52,11 @@ public partial class TeamMemberViewModel(TeamMember member) : ObservableObject
         OnPropertyChanged(nameof(Tasks));
     }
 
-    private static ImageSource? DecodeImage(byte[]? bytes, string initial)
+    private static ImageSource? DecodeImage(byte[]? bytes, string fallback)
     {
         if (bytes is null || bytes.Length == 0)
         {
-            var idx = Math.Abs(initial.GetHashCode()) % MaxPlaceholderProfiles;
+            var idx = Math.Abs(fallback.GetHashCode()) % MaxPlaceholderProfiles;
             var placeholder = ImageSource.FromFile($"placeholder_profile_{idx}.png");
             return placeholder;
         }
@@ -59,6 +64,52 @@ public partial class TeamMemberViewModel(TeamMember member) : ObservableObject
         var source = ImageSource.FromStream(() => new MemoryStream(bytes));
 
         return source;
+    }
+
+    private static Color DecodeColor(byte[]? profilePictureBytes)
+    {
+        if (profilePictureBytes is null || profilePictureBytes.Length == 0)
+        {
+            return Colors.Black;
+        }
+
+        var best = GetBestColor(profilePictureBytes);
+
+        return best.ToMauiColor();
+    }
+
+    private static SKColor GetBestColor(byte[] bytes)
+    {
+        var colors = GetColors(bytes).ToArray();
+
+        var r = colors.Sum(c => c.Red);
+        var g = colors.Sum(c => c.Green);
+        var b = colors.Sum(c => c.Blue);
+
+        var count = colors.Length;
+
+        var best = new SKColor(
+            (byte)(r / count),
+            (byte)(g / count),
+            (byte)(b / count));
+
+        return best;
+    }
+
+    private static IEnumerable<SKColor> GetColors(byte[] bytes)
+    {
+        using var image = SKImage.FromEncodedData(bytes);
+        using var loaded = image.ToRasterImage(true);
+        using var pixmap = loaded.PeekPixels();
+
+        for (var x = 0; x < image.Width; x++)
+        {
+            for (var y = 0; y < image.Height; y++)
+            {
+                var color = pixmap.GetPixelColor(x, y);
+                yield return color;
+            }
+        }
     }
 
     private static ObservableCollection<TaskItemViewModel> GetTasks(IList<TaskItem>? tasks)
